462453e85863e7c85a2b311af9e7c1031f36bcbf
José Leal <lealnetosena@gmail.com>
2024-12-18 15:35:16 -0300

    configuração inicial spring

---
+++ src/test/java/br/com/voto/VotoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.voto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
